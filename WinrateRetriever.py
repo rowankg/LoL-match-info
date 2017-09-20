57304b87160de604d47fdc6829745385ae03f14f
Rowan Klein Gunnewiek <rowankleingunnewiek@hotmail.com>
2017-09-20 20:51:13 +0200

    add get_match_history_data

--- WinrateRetriever.py
+++ WinrateRetriever.py
@@ -1,23 +1,24 @@
 import requests, json
 import requests.exceptions
 
-
-api_token = "RGAPI-fda5cdb6-a2e6-4726-aa04-ebefb203f01e"
+api_token = "RGAPI-ee86b79a-294f-436c-940b-341086c8cdb4"
 api_base_url = "https://euw1.api.riotgames.com/lol/"
+summoner_name = "Justaz"
+season = 9
 
 class Summoner:
-
     def __init__(self, summoner_id=0, champion_id=0, summoner_name=0, team=0):
         self.summoner_name = summoner_name
         self.summoner_id = summoner_id
         self.champion_id = champion_id
         self.team = team
 
-    def set_summoner_data(self, summoner_id, champion_id, summoner_name, team):
+    def set_summoner_data(self, summoner_id, champion_id, summoner_name, team, account_id):
         self.summoner_name = summoner_name
         self.summoner_id = summoner_id
         self.champion_id = champion_id
         self.team = team
+        self.account_id = account_id
 
     def set_match_data(self, game_id, total_games, games_won):
         self.game_id = game_id
@@ -30,21 +31,26 @@ def get_account_id(summonerName):
     account_id = r.json()['accountId']
     return account_id
 
+
 def get_summoner_id(summonerName):
     r = requests.get(createUrl(summonerName))
+    print(createUrl(summonerName))
     summoner_id = r.json()['id']
     return summoner_id
+
 
 def createUrl(summonerName):
     url = "https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + summonerName + "?api_key=" + api_token
     return url
 
-def createUrl2 (*args):
+
+def createUrl2(*args):
     url = api_base_url
     for arg in args:
         url = url + str(arg)
     url = url + "?api_key=" + api_token
     return url
+
 
 def createUrl3(api_path, *input_params):
     """
@@ -64,6 +70,7 @@ def createUrl3(api_path, *input_params):
     url = url + "?api_key=" + api_token
     return url
 
+
 def get_champion_by_id(champion_id):
     url = createUrl3("static-data/v3/champions/", champion_id)
     r = requests.get(url)
@@ -73,7 +80,8 @@ def get_champion_by_id(champion_id):
 
 
 def get_current_match(summoner_id):
-    url = "https://euw1.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/" + str(summoner_id) + "?api_key=" + api_token
+    url = "https://euw1.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/" + str(
+        summoner_id) + "?api_key=" + api_token
     r = requests.get(url)
     participants = r.json()['participants']
     summonerData = []
@@ -82,14 +90,17 @@ def get_current_match(summoner_id):
         participant_id = participant['summonerId']
         champion_id = participant['championId']
         team = participant['teamId']
+        account_id = get_account_id(summoner_name)
 
-        champion_name = get_champion_by_id(champion_id)
 
         summoner = Summoner()
-        summoner.set
+        summoner.set_summoner_data(summoner_id, champion_id, participant_id, team, account_id)
+
+        # get match history for every participant
+        get_match_history_by_account_id(summoner, season, account_id, champion_id)
 
         summonerData.append(summoner)
-        print(summoner_name, participant_id, champion_id, team)
+        print(summoner_name, participant_id, champion_id, team, account_id)
     return summonerData
 
 
@@ -97,13 +108,31 @@ def get_champion_data(championId):
     return 0
 
 
-def main():
-    createUrl2("platform/v3/champions/", 90)
-    createUrl3("platform/v3/champions/", 90)
+def get_match_history_by_account_id(summoner, season, account_id, champion_id):
+    url = "https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/" + str(account_id) + "?season=" + str(season) +\
+          "&champion=" + str(champion_id) + "&api_key=" + api_token
 
-    summoner_id = get_summoner_id("ItzRowan")
+    r = requests.get(url)
+    game_id = []
+    if r.ok:
+        for match in r.json()["matches"]:
+            game_id.append((match['gameId']))
+            # make url for every match with match id and check if won or not,
+            #if match won, games_won ++
+        total_games = r.json()['totalGames']
+        print(total_games)
+    else:
+        game_id = []
+        total_games = 0
+
+
+
+        # summoner.set_match_data(game_id, total_games, games_won)
+
+def main():
+    summoner_id = get_summoner_id(summoner_name)
     print(summoner_id)
-    account_id = get_account_id("ItzRowan")
+    account_id = get_account_id(summoner_name)
     print(account_id)
     get_current_match(summoner_id)
 
